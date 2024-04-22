5d19a540173ade3dbf6fa4c27b03f81946257ca7
Carla Bernardino <carla.matos.bernardino@celfocus.com>
2024-04-22 12:15:26 +0100

    Defining PlanCard final layout, finalizing Pricing component

--- src/Pricing.tsx
+++ src/Pricing.tsx
@@ -1,5 +1,4 @@
 import "./pricing.scss";
-import Button from "./components/Button/Button";
 import Text from "./components/Text/Text";
 import Heading from "./components/Heading/Heading";
 import Toggle from "./components/Toggle/Toggle";
@@ -19,6 +18,41 @@ function Pricing() {
         <Text type="p" style="tinted">
           Monthly
         </Text>
+      </div>
+      <div className="pricing__plans">
+        <PlanCard
+          style={"white"}
+          name={{ type: "p", style: "normal", children: "Basic" }}
+          price={{ level: "h1", style: "normal", children: "199.99" }}
+          features={[
+            { type: "p", style: "normal", children: "500 GB Storage" },
+            { type: "p", style: "normal", children: "2 Users Allowed" },
+            { type: "p", style: "normal", children: "Send up to 3 GB" },
+          ]}
+          button={{ style: "primary", children: "LEARN MORE" }}
+        />
+        <PlanCard
+          style={"purple"}
+          name={{ type: "p", style: "normal", children: "Professional" }}
+          price={{ level: "h1", style: "normal", children: "249.99" }}
+          features={[
+            { type: "p", style: "normal", children: "1 TB Storage" },
+            { type: "p", style: "normal", children: "5 Users Allowed" },
+            { type: "p", style: "normal", children: "Send up to 10 GB" },
+          ]}
+          button={{ style: "secondary", children: "LEARN MORE" }}
+        />
+        <PlanCard
+          style={"white"}
+          name={{ type: "p", style: "normal", children: "Master" }}
+          price={{ level: "h1", style: "normal", children: "399.99" }}
+          features={[
+            { type: "p", style: "normal", children: "2 TB Storage" },
+            { type: "p", style: "normal", children: "10 Users Allowed" },
+            { type: "p", style: "normal", children: "Send up to 20 GB" },
+          ]}
+          button={{ style: "primary", children: "LEARN MORE" }}
+        />
       </div>
     </div>
   );
